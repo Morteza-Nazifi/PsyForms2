539a516e043888c0efd01e43d7f98d89fb505816
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-17 19:15:53 +0330

    Update Shape.kt

--- app/src/main/java/ir/psyforms/app/ui/theme/Shape.kt
+++ app/src/main/java/ir/psyforms/app/ui/theme/Shape.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package vtsen.hashnode.dev.newemptycomposeapp.ui.theme
+package ir.psyforms.app.ui.theme
 
 import androidx.compose.material3.Shapes
 
