3a9918ba8f601fe8bacda4ede40245507392410a
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-19 08:33:08 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -70,4 +71,9 @@ dependencies {
     implementation(libs.androidx.splash.screen)
 
     implementation(libs.vinchamp77.buildutils)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
